33176d6fb6436861bf6a2b0e0e918d9bbd5f2219
Victor Costa <antonio.passos@grupomateus.com.br>
2023-06-02 11:33:41 -0300

    adicionando PrintService

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,40 @@
-import model.Produto;
 import service.ProdutoService;
+import service.PrintService;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        PrintService printUtil = new PrintService();
         ProdutoService produtoService = new ProdutoService();
-        Produto pd1 = new Produto("coxinha", 2.00, "fastfood", 0);
-        Produto pd2 = new Produto("Pizza", 55.00, "fastfood", 1);
-        Produto pd3 = new Produto("Lasanha", 4.00, "fastfood", 2);
+        Scanner scanner = new Scanner(System.in);
+        int option;
 
-        //Lista de produtos vazia
-        System.out.println(produtoService.listarProdutos());
-        produtoService.adicionarProduto(pd1);
-        produtoService.adicionarProduto(pd2);
-        produtoService.adicionarProduto(pd3);
+        printUtil.boasVindas();
 
-        //Lista com os produtos adicionados
-        System.out.println(produtoService.listarProdutos());
+        do {
+            printUtil.printOptions();
+            option = scanInt(scanner);
 
-        //Lista com produtos removidos
-        produtoService.deletarProduto(0);
-        System.out.println(produtoService.listarProdutos());
+            if (option < 0 || option > 4) {
+                continue;
+            }
+        } while (option != 0);
 
-        //Lista com produtos alterados
-        Produto produtoAlterar = new Produto(1);
-        produtoAlterar.setPreco(22D);
-
-        produtoService.alterarProduto(produtoAlterar);
-
-        System.out.println(produtoService.listarProdutos());
-
+        printUtil.saindo();
     }
+
+
+    public static int scanInt(Scanner scanner) {
+        try {
+            if (scanner.hasNextInt()) {
+                return Integer.parseInt(scanner.nextLine());
+            }
+            scanner.nextLine();
+            return -1;
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
 }
---
+++ src/service/PrintService.java
@@ -0,0 +1,45 @@
+package service;
+
+public class PrintService {
+
+    public void boasVindas() {
+        printLoopWithSleep("=", 10, 30);
+        printMessageWithSleep("\nBem vindo ao Find-Service\n", 200);
+        printLoopWithSleep("=", 10, 30);
+    }
+    public void printOptions() {
+        printMessageWithSleep("\n.", 320);
+        printMessageWithSleep("\nEscolha uma opção e tecle Enter:  \n", 400);
+        printMessageWithSleep("1 - Listar Produtos\n", 250);
+        printMessageWithSleep("2 - Adicionar um Produto\n", 250);
+        printMessageWithSleep("3 - Remover um Produto\n", 250);
+        printMessageWithSleep("4 - Mostrar um Produto\n", 250);
+        printMessageWithSleep("0 - Sair\n", 250);
+    }
+
+    public void saindo() {
+        printMessageWithSleep("Saindo", 100);
+        printLoopWithSleep(".", 420, 3);
+        printMessageWithSleep("\nAté Breve!\n", 550);
+    }
+
+    private void printLoopWithSleep(String msg, int timer, int quantity) {
+        try {
+            for(int i = 0; i < quantity; i++){
+                Thread.sleep(timer);
+                System.out.print(msg);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void printMessageWithSleep(String msg, int timer) {
+        try {
+             Thread.sleep(timer);
+             System.out.print(msg);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
